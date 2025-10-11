6a746aa05577310e7c1d02da4fc2ce29481f9fe7
twbc07 <twbc07@gmail.com>
2025-10-11 18:01:49 +0900

    編集機能作成

--- resources/views/livewire/memos/create.blade.php
+++ resources/views/livewire/memos/create.blade.php
@@ -8,10 +8,12 @@ state(['title', 'body']);
 // メモを保存する関数
 $store = function () {
     // フォームからの入力値をデータベースへ保存
-    Memo::create([
-        'title' => $this->title,
-        'body' => $this->body,
-    ]);
+    // Memo::create([$this->all())
+    //     'title' => $this->title,
+    //     'body' => $this->body,
+    // ]);
+
+    Memo::create($this->all());
     // 一覧ページにリダイレクト
     return redirect()->route('memos.index');
 };
