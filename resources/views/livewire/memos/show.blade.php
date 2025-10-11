94f710285ed38bf87e545c6b51e53f0c443c872e
twbc07 <twbc07@gmail.com>
2025-10-11 18:11:40 +0900

    削除機能作成

--- resources/views/livewire/memos/show.blade.php
+++ resources/views/livewire/memos/show.blade.php
@@ -9,6 +9,11 @@ state(['memo' => fn(Memo $memo) => $memo]);
 $edit = function () {
     return redirect()->route('memos.edit', $this->memo);
 };
+$destroy = function () {
+    $this->memo->delete();
+    return redirect()->route('memos.index');
+};
+
 ?>
 
 <div>
@@ -17,4 +22,6 @@ $edit = function () {
     <p>{!! nl2br(e($memo->body)) !!}</p>
 
     <button wire:click="edit">編集する</button>
+    <button wire:click="destroy"wire:confirm="本当に削除しますか?">削除する</button>
+
 </div>
