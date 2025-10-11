104ee613016a2292148aa01ce71f0129a7b1247e
twbc07 <twbc07@gmail.com>
2025-10-11 15:11:12 +0900

    メモの照会機能作成

---
+++ resources/views/livewire/memos/show.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use function Livewire\Volt\{state};
+use App\Models\Memo;
+
+// ルートモデルバインディング
+state(['memo' => fn(Memo $memo) => $memo]);
+
+?>
+
+<div>
+    <h1>{{ $memo->title }}</h1>
+    <p>{!! nl2br(e($memo->body)) !!}</p>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 詳細ページ
+Volt::route('/memos/{memo}', 'memos.show')->name('memos.show');
