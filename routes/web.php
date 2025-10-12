1d5837e6fb9ad97b2092d0317ab8522843c83417
twbc07 <twbc07@gmail.com>
2025-10-12 12:11:53 +0900

    Revert "Feature"

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Livewire\Volt\Volt;
 
 Route::get('/', function () {
     return view('welcome');
 });
-// 一覧ページ
-Volt::route('/memos', 'memos.index')->name('memos.index');
-
-// 新規登録ページ
-Volt::route('/memos/create', 'memos.create')->name('memos.create');
-
-Volt::route('/memos/{memo}', 'memos.show')->name('memos.show');
-
-// 編集ページ
-Volt::route('/memos/{memo}/edit', 'memos.edit')->name('memos.edit');
