4fd21ceafd6a796991ed54346f90ad4af390e0fc
twbc07 <twbc07@gmail.com>
2025-10-12 09:35:03 +0900

    バリデーション実装

--- resources/views/livewire/memos/create.blade.php
+++ resources/views/livewire/memos/create.blade.php
@@ -1,9 +1,16 @@
 <?php
 
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{state, rules};
 use App\Models\Memo;
 
 state(['title', 'body']);
+
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:2000',
+]);
+
+// バリデーションルールを定義
 
 // メモを保存する関数
 $store = function () {
@@ -12,7 +19,7 @@ $store = function () {
     //     'title' => $this->title,
     //     'body' => $this->body,
     // ]);
-
+    $this->validate(); // バリデーションチェック
     Memo::create($this->all());
     // 一覧ページにリダイレクト
     return redirect()->route('memos.index');
@@ -27,12 +34,20 @@ $store = function () {
     <!-- wire:submit="store"でフォーム送信時にstore関数を呼び出し -->
     <form wire:submit="store">
         <p>
-            <label for="title">タイトル</label><br>
+            <label for="title">タイトル</label>
+            @error('title')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <!-- wire:model="title"で入力値とコンポーネントの状態($this->title)を自動的に同期 -->
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
-            <label for="body">本文</label><br>
+            <label for="body">本文</label>
+            @error('body')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <!-- wire:model="body"で入力値とコンポーネントの状態($this->body)を自動的に同期 -->
             <textarea wire:model="body" id="body"></textarea>
         </p>
--- resources/views/livewire/memos/edit.blade.php
+++ resources/views/livewire/memos/edit.blade.php
@@ -1,6 +1,6 @@
 <?php
 
-use function Livewire\Volt\{state, mount};
+use function Livewire\Volt\{state, mount, rules};
 use App\Models\Memo;
 
 state(['memo', 'title', 'body']);
@@ -11,7 +11,14 @@ mount(function (Memo $memo) {
     $this->body = $memo->body;
 });
 
+// バリデーションルールを定義
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:2000',
+]);
+
 $update = function () {
+    $this->validate(); // バリデーションチェック
     $this->memo->update($this->all());
     return redirect()->route('memos.show', $this->memo);
 };
@@ -24,11 +31,19 @@ $update = function () {
 
     <form wire:submit="update">
         <p>
-            <label for="title">タイトル</label><br>
+            <label for="title">タイトル</label>
+            @error('title')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
-            <label for="body">本文</label><br>
+            <label for="body">本文</label>
+            @error('body')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <textarea wire:model="body" id="body"></textarea>
         </p>
         <button type="submit">更新</button>
