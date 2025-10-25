022f0b97c266bc57be3148966c08f01242409f1d
twbc07 <twbc07@gmail.com>
2025-10-25 11:24:57 +0900

    新一のおかげ

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
-echo "Running composer"
-composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+echo "Installing dependencies..."
+composer install --no-dev --optimize-autoloader
 
-echo "Caching config..."
+echo "Caching configuration..."
 php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
 
+echo "Caching views..."
+php artisan view:cache
+
+echo "Publishing Livewire assets..."
+php artisan livewire:publish --assets
+
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Installing frontend dependencies..."
+npm install
+
+echo "Building frontend assets..."
+npm run build
